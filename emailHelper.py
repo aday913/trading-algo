10a58d01a972407de008113329789e16de901940
aday913 <45890246+aday913@users.noreply.github.com>
2021-03-13 21:07:43 -0700

    Allow for the reading of emails in the past day

--- emailHelper.py
+++ emailHelper.py
@@ -1,6 +1,8 @@
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import smtplib, ssl
+import imapclient
+import datetime
 import email
 import os
 
@@ -20,6 +22,8 @@ class Emailer(object):
         self.RECEIVER = os.getenv('RECEIVEREMAIL')
         self.PASSWORD = os.getenv('EMAILPASSWORD')
 
+        self.seenIDs = []
+
     def sendMessage(self, message, subject='Weekly Update'):
         '''
         '''
@@ -36,6 +40,35 @@ class Emailer(object):
             server.sendmail(self.SENDER, self.RECEIVER, msg.as_string())
         print('Successfully sent the email!')
 
+    def readEmail(self):
+        '''
+        Brings in unread emails and parses out the body of the email if the
+        email is from self.RECEIVER
+        '''
+        imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
+        imapObj.login(self.SENDER, self.PASSWORD)
+        imapObj.select_folder('INBOX', readonly=True)
+
+        now = datetime.datetime.now()
+        yesterday = (now + datetime.timedelta(days=-1)).strftime('%d-%b-%Y')
+        UIDs = imapObj.search([u'SINCE', yesterday, u'UNSEEN'])
+        messages = []
+        for i in UIDs:
+            if i not in self.seenIDs:
+                rawMessage = imapObj.fetch(i, 'BODY[]')
+                msg = email.message_from_bytes(rawMessage[i][b'BODY[]'])
+                if msg['from'].split()[-1] == '<{}>'.format(self.RECEIVER):
+                    if msg.is_multipart:
+                        multi = msg.get_payload()
+                        for j in multi:
+                            temp = j.get_payload()
+                            if temp[0] != '<':
+                                messages.append(j.get_payload()[:-2])
+                    else:
+                        messages.append(msg.get_payload()[:-2])
+                self.seenIDs.append(i)
+        return messages
+
 if __name__ == '__main__':
     emailBot = Emailer()
     emailBot.sendMessage(
@@ -43,3 +76,4 @@ if __name__ == '__main__':
             Hey there! This is a test email from adaypython!
         '''
         )
+    print(emailBot.readEmail())
