92025c6e6f02e407b1f9f23e4b769b7ceab96276
Alex Day <alexdaygjj@gmail.com>
2024-02-10 11:00:40 +0000

    Formatting

--- backtest.py
+++ backtest.py
@@ -10,45 +10,44 @@ import datetime
 import logging
 import os
 
+
 class Portfolio(object):
-    '''
+    """
     Portfolio object that will keep track of the various assets that
     we buy/sell, as well as track the equity available
-    '''
+    """
+
     def __init__(self, api, debug=False):
         if debug:
-            logging.basicConfig(level=logging.DEBUG,
-                                format='%(name)s: %(message)s')
+            logging.basicConfig(level=logging.DEBUG, format="%(name)s: %(message)s")
         else:
-            logging.basicConfig(level=logging.INFO,
-                                format='%(name)s: %(message)s')
-        
-        if api == 'own':
+            logging.basicConfig(level=logging.INFO, format="%(name)s: %(message)s")
+
+        if api == "own":
             load_dotenv()
 
-            APCA_API_SECRET_KEY = os.getenv('APCA_API_SECRET_KEY')
-            APCA_API_KEY_ID = os.getenv('APCA_API_KEY_ID')
-            APCA_API_BASE_URL = os.getenv('APCA_API_BASE_URL')
+            APCA_API_SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
+            APCA_API_KEY_ID = os.getenv("APCA_API_KEY_ID")
+            APCA_API_BASE_URL = os.getenv("APCA_API_BASE_URL")
 
             self.api = tradeapi.REST()
         else:
             self.api = api
 
+
 class Backtest(object):
     def __init__(self, api, strategy, debug=False):
         if debug:
-            logging.basicConfig(level=logging.DEBUG,
-                                format='%(name)s: %(message)s')
+            logging.basicConfig(level=logging.DEBUG, format="%(name)s: %(message)s")
         else:
-            logging.basicConfig(level=logging.INFO,
-                                format='%(name)s: %(message)s')
+            logging.basicConfig(level=logging.INFO, format="%(name)s: %(message)s")
 
-        if api == 'own':
+        if api == "own":
             load_dotenv()
 
-            APCA_API_SECRET_KEY = os.getenv('APCA_API_SECRET_KEY')
-            APCA_API_KEY_ID = os.getenv('APCA_API_KEY_ID')
-            APCA_API_BASE_URL = os.getenv('APCA_API_BASE_URL')
+            APCA_API_SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
+            APCA_API_KEY_ID = os.getenv("APCA_API_KEY_ID")
+            APCA_API_BASE_URL = os.getenv("APCA_API_BASE_URL")
 
             self.api = tradeapi.REST()
         else:
@@ -62,9 +61,9 @@ class Backtest(object):
         backtestable = {}
         i = 0
         for asset in initial:
-            logging.debug('Grabbing {} of {} assets...'.format(i, len(initial)))
-            if asset.status == 'active' and asset.fractionable:
-                temp = self.api.get_barset(asset.symbol, '1D', limit=1000)
+            logging.debug("Grabbing {} of {} assets...".format(i, len(initial)))
+            if asset.status == "active" and asset.fractionable:
+                temp = self.api.get_barset(asset.symbol, "1D", limit=1000)
                 prices = []
                 for i in temp[asset.symbol]:
                     prices.append(temp[asset.symbol])
@@ -72,7 +71,7 @@ class Backtest(object):
                     backtestable[asset.symbol] = prices
             i += 1
         return backtestable
-        
 
-if __name__ == '__main__':
-    test = Backtest(api='own', strategy=None, debug=True)
+
+if __name__ == "__main__":
+    test = Backtest(api="own", strategy=None, debug=True)
